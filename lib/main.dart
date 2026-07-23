44e9e51ed9dbb9456bf95692e52d76710a414e8a
Ruthwika <24wh1a0540@bvrithyderabad.edu.in>
2026-07-23 11:03:37 +0530

    new update

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(
-            "Hello Flutter",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
-              fontStyle: FontStyle.italic,
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Hello Flutter",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Button was clicked"),
+                    ),
+                  );
+                },
+                child: const Text("Click Me"),
+              ),
+            ],
           ),
         ),
       ),
